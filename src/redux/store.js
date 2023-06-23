e23b23a84bbc5c9c4fec14fd14f62745b2a58c6a
Donald Akite <donaldakite27@gmail.com>
2023-06-23 11:42:48 +0000

    Add mentor to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice/userSlice';
+import mentorReducer from './mentor/mentorReducer';
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    mentor: mentorReducer,
   },
 });
