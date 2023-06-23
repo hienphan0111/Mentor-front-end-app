27bf3fc0792771a7a6eb91d98d6187c8ddd59b58
Chris <hakizachrist1@gmail.com>
2023-06-23 16:26:04 +0200

    Update app.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice/userSlice';
+import reserveReducer from './reservation/reservationSlice';
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    reservation: reserveReducer
   },
 });
