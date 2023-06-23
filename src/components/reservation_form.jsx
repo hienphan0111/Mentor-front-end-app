89e92356924f86c2afcf9583722ba7710b28e590
Chris <hakizachrist1@gmail.com>
2023-06-23 14:51:32 +0200

    Update app.css

--- src/components/reservation_form.jsx
+++ src/components/reservation_form.jsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react';
 import { useSelector, useDispatch} from 'react-redux';
 import { addreserve } from '../redux/reservation/reservationSlice';
 import { fetchreservation } from '../redux/reservation/reservationSlice';
+import DateTimePicker from 'react-datetime-picker';
 
 const initials = {
-  time: '',
+  datetime: '',
   message: '',
 };
 
@@ -18,15 +19,14 @@ const Form = () => {
   }, [dispatch]);
  
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = (name, value) => {
     setState((prev) => ({
       ...prev, [name]: value
     }));
   };
 
   const handleSubmit = () => {
-    dispatch(fetchreservation());
+    dispatch(addreserve(state));
     setState(initials);
   };
 
@@ -36,12 +36,20 @@ const Form = () => {
       <div className="Title">
         Add your reservation
       </div>
+  
       <form className="Form" method="post">
-        <input type="time" value={state.time} id="time" className="form_item" name="time" placeholder="Time" onChange={handleChange} required />
-        <input type="text" value={state.message} id="message" className="form_item" name="message" placeholder="message" onChange={handleChange} required />
+        <div className="form_item">
+          <label htmlFor="datetime">Date and Time:</label>
+          <DateTimePicker id="datetime" name="datetime" onChange={(value) => handleChange("datetime", value)} value={state.datetime} required/>
+        </div>
+        <div className="form_item">
+          <input type="text" value={state.message} id="message" name="message" placeholder="message" onChange={(e) => handleChange(e.target.name, e.target.value)} required />
+        </div>
         <button type="button" onClick={handleSubmit} className="form_button">Submit</button>
       </form>
+    
     </div>
   );
 };
+
 export default Form;
