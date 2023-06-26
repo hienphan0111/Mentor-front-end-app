3d7b90d4792af80b412a17fa2d6a41490d8126ba
Donald Akite <donaldakite27@gmail.com>
2023-06-26 17:28:19 +0000

    Fix width to full screen

--- src/components/mentor_form.jsx
+++ src/components/mentor_form.jsx
@@ -28,8 +28,8 @@ function MentorForm() {
   };
 
   return (
-    <div>
-      <h2>Create Mentor</h2>
+    <div className="mentor-form-container w-full h-screen">
+      <h1>Create Mentor</h1>
       {error && (
       <p>
         Error:
@@ -41,28 +41,28 @@ function MentorForm() {
         {mentor.name}
       </p>
       )}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name" className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+      <form onSubmit={handleSubmit} className="mentor-form flex flex-col justify-center items-center">
+        <label htmlFor="name" className="w-full text-center block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4 max-w-[400px]">
           Name
           <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
         </label>
         <br />
-        <label htmlFor="bio" className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+        <label htmlFor="bio" className="w-full text-center block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4 max-w-[400px]">
           Bio
           <textarea id="bio" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={bio} onChange={(e) => setBio(e.target.value)} />
         </label>
         <br />
-        <label htmlFor="photo" className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+        <label htmlFor="photo" className="w-full block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4 max-w-[400px]">
           Photo
           <input type="file" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="photo" value={photo} onChange={(e) => setPhoto(e.target.value)} />
         </label>
         <br />
-        <label htmlFor="contact" className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+        <label htmlFor="contact" className="w-full block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4 max-w-[400px]">
           Contact
           <input type="text" id="contact" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={contact} onChange={(e) => setContact(e.target.value)} />
         </label>
         <br />
-        <button type="submit" className="shadow bg-purple-500 hover:bg-purple-400 w-full focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">create Mentor</button>
+        <button type="submit" className="w-full max-w-[400px] shadow bg-purple-500 hover:bg-purple-400 w-full focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">create Mentor</button>
       </form>
     </div>
   );
